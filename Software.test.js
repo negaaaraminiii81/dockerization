986db7700a0ca60ce4f99ca22578d38f5fb2e9d9
negaaaraminiii81 <negaaraminii@gmail.com>
2023-06-10 20:49:10 +0430

    new gcd test

--- Software.test.js
+++ Software.test.js
@@ -19,3 +19,8 @@ test('gcd of 81 and 27 is ', () => {
 test('gcd of 100 and 20 is ', () => {
   expect(gcd.gcd(100, 20)).toBe(20);
 });
+
+test('gcd of 21 and 13 is ', () => {
+  expect(gcd.gcd(21, 13)).toBe(1);
+});
+
